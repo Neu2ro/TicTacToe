00a2764cf91678491fd0101fdfb957e500c06ed1
nagpa <jaynagpal02@gmail.com>
2022-12-10 03:30:20 +0530

    Back buttons function added

--- app/src/main/java/com/example/tictactoe/GridThreeDashboard.java
+++ app/src/main/java/com/example/tictactoe/GridThreeDashboard.java
@@ -2,15 +2,30 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class GridThreeDashboard extends AppCompatActivity {
+
+    ImageButton threetodash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_three_dashboard);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        threetodash = findViewById(R.id.threetodash);
+
+        threetodash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GridThreeDashboard.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
