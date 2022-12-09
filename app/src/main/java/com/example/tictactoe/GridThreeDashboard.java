a1de3617a168430581336480efb6c8442192fd66
nagpa <jaynagpal02@gmail.com>
2022-12-10 03:37:57 +0530

    Three Offline Multiplayer activity created

--- app/src/main/java/com/example/tictactoe/GridThreeDashboard.java
+++ app/src/main/java/com/example/tictactoe/GridThreeDashboard.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class GridThreeDashboard extends AppCompatActivity {
 
     ImageButton threetodash;
+    Button offlinemulti3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,20 @@ public class GridThreeDashboard extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         threetodash = findViewById(R.id.threetodash);
+        offlinemulti3 = findViewById(R.id.offlinemulti3);
 
         threetodash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(GridThreeDashboard.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
+
+        offlinemulti3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GridThreeDashboard.this, ThreeOfflineMultiplayer.class);
                 startActivity(intent);
             }
         });
