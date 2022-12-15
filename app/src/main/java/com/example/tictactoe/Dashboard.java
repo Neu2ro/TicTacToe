69155448898b7993664b45656239d701aa01d162
vivek <vivekbalani1@gmail.com>
2022-12-15 21:42:34 +0530

    Updated

--- app/src/main/java/com/example/tictactoe/Dashboard.java
+++ app/src/main/java/com/example/tictactoe/Dashboard.java
@@ -26,7 +26,7 @@ public class Dashboard extends AppCompatActivity {
         gridThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Dashboard.this, GridThreeDashboard.class);
+                Intent intent = new Intent(Dashboard.this, ThreeOfflineMultiplayer.class);
                 startActivity(intent);
             }
         });
@@ -34,7 +34,7 @@ public class Dashboard extends AppCompatActivity {
         gridNine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Dashboard.this, GridNineDashboard.class);
+                Intent intent = new Intent(Dashboard.this, NineOfflinemultiplayer.class);
                 startActivity(intent);
             }
         });
